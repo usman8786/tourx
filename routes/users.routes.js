7ccc67089ab758fe852a7a8364b29366693ddf09
usman8786 <usman.mushtaq8786@gmail.com>
2021-02-11 10:38:59 +0500

    route for get all user added

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const UserController = require("../controllers/users.controllers");
 // const checkAuth = require('../middleware/check-auth');
 
+router.get("/", UserController.getAllUsers);
 router.post("/login", UserController.loginUser);
 router.post("/register", UserController.registerUser);
 router.put("/:_id", UserController.updateUser);
